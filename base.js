a997f8092f554d081c2d483e9ae1a27df4d0dd9c
danlyg <gih.daniel@gmail.com>
2015-05-11 17:44:51 -0700

    got all the gifs added

--- base.js
+++ base.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
   // code in here!
   console.log("here's your data:", mockData)
-})
+
+
+  function render(theData){
+		$.each(mockData.data, function(){
+			$('body').append("<img src='" 
+								+ this.images.fixed_height.url
+								+ "'>");
+		});
+	};
+ 	render(mockData);
+});
